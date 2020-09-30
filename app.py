edeabc6e6917b1101389860c2b2e64d25d4defe6
sjameson-jhcs <sjameson@jhcschool.org>
2020-09-30 13:53:35 -0600

    Added psuedocode for build along

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
-print("Hello, World!")
+
+
+# Get a string from the user that needs to be encrypted
+
+# Convert the string into a list of ASCII numbers
+
+# Convert the ASCII numbers into binary
+
+# Define an encryption key
+
+# Do binary addition
+
+# Attempt to convert encrypted binary to normal characters?
+
+# Use encryption key to unscramble the binary
+
+# Convert binary back to known characters
+
+
+
+
+######## ADD ONS ########
+# 1. Attempt to brute force check encryption keys
+# 2. Show python built in MD5 hash generator hashlib.md5(b'blah'))
