0ee3f4ff5b2e1f986c51d1eebfaf6e669039435e
sjameson-jhcs <sjameson@jhcschool.org>
2020-09-30 20:44:14 -0600

    Added psuedocode for build along

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
 
 
 # Get a string from the user that needs to be encrypted
+string_to_encrypt = input("What do you want to encrypt? ")
+
 
 # Convert the string into a list of ASCII numbers
+# Convert the string_to_encrypt into a list of charcters
+list_to_encrypt = list(string_to_encrypt)
+# Convert each character into an ASCII number by looping through all characters
+i = 0
+for character in list_to_encrypt:
+    list_to_encrypt[i] = ord(character)
+    i += 1
+
 
 # Convert the ASCII numbers into binary
+k = 0
+binary_converted_values = []
+for ascii_character in list_to_encrypt:
+    binary_converted_values.append("")
+    for i in range(8):
+        j = 7 - i
+        if ascii_character >= 2 ** j:
+            ascii_character -= 2 ** j
+            binary_converted_values[k] = binary_converted_values[k] + "1"
+        else:
+            binary_converted_values[k] = binary_converted_values[k] + "0"
+    k += 1
 
+print(binary_converted_values)
 # Define an encryption key
 
 # Do binary addition
